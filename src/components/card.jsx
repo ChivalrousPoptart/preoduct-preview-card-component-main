4a768e3cd3baa3c219367a1589ec087c659e1fde
Clayton <claytonvarnon@ymail.com>
2022-08-28 14:43:46 -0500

    feat: adds image into card, moves images folder

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,12 @@
 
 const Card = () => {
-    return <p>I made this change!</p>;
+    // need to
+    return <div>
+        <img src={require('../images/image-product-desktop.jpg')} alt="Picture of Perfume" width="500" height="600" />
+        <form>
+            <p>I made this change!</p>
+        </form>
+    </div>;
 }
 
 export default Card;
